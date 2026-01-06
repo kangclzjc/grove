a598d36788f6d670bcbea7460a4faf02715f5f30
kangclzjc <kangz@nvidia.com>
2026-01-06 11:28:35 +0800

    remove unused
    Signed-off-by: kangclzjc <kangz@nvidia.com>

--- operator/internal/schedulerbackend/controller/register.go
+++ operator/internal/schedulerbackend/controller/register.go
@@ -20,6 +20,7 @@ import (
 	"fmt"
 
 	"github.com/ai-dynamo/grove/operator/internal/schedulerbackend"
+
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
